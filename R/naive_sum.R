3ddc628c579f8c8b6d5ac31256d63e689f022731
Guido Alberti Moreira <42647037+GuidoAMoreira@users.noreply.github.com>
2021-03-29 14:34:05 -0300

    Add files via upload

--- R/naive_sum.R
+++ R/naive_sum.R
@@ -1,9 +1,9 @@
 #' Test function
 #'
-#' Wraps the adapt_sum C function for testing
+#' Wraps the naive_sum C function for testing
 #' @importFrom matrixStats logSumExp
 #' @export
-adapt_sum <- function(lFun, params = numeric(), eps = 1e-15, maxIter = 1e5,
+naive_sum <- function(lFun, params = numeric(), eps = 1e-15, maxIter = 1e5,
                       logL = -Inf, n0 = 0){
 
   maxIter = as.integer(maxIter)
@@ -13,47 +13,47 @@ adapt_sum <- function(lFun, params = numeric(), eps = 1e-15, maxIter = 1e5,
 
   if (is.character(lFun)){
     if (lFun == "negbin_marginal")
-      out = .Call("adapt_sum_callPrecomp",
-                  1L, params, eps, maxIter, logL, n0,
+      out = .Call("naive_sum_callPrecomp",
+                  1L, params, eps, maxIter, n0,
                   PACKAGE = "adaptiveSum")
     else if (lFun == "noObs")
-        out = .Call("adapt_sum_callPrecomp",
-                    2L, params, eps, maxIter, logL, n0,
-                    PACKAGE = "adaptiveSum")
+      out = .Call("naive_sum_callPrecomp",
+                  2L, params, eps, maxIter, n0,
+                  PACKAGE = "adaptiveSum")
     else if (lFun == "COMP")
-      out = .Call("adapt_sum_callPrecomp",
-                  3L, params, eps, maxIter, logL, n0,
+      out = .Call("naive_sum_callPrecomp",
+                  3L, params, eps, maxIter, n0,
                   PACKAGE = "adaptiveSum")
     else if (lFun == "dR0")
-      out = .Call("adapt_sum_callPrecomp",
-                  4L, params, eps, maxIter, logL, n0,
+      out = .Call("naive_sum_callPrecomp",
+                  4L, params, eps, maxIter, n0,
                   PACKAGE = "adaptiveSum")
     else if (lFun == "PL_diff")
-      out = .Call("adapt_sum_callPrecomp",
-                  5L, params, eps, maxIter, logL, n0,
+      out = .Call("naive_sum_callPrecomp",
+                  5L, params, eps, maxIter, n0,
                   PACKAGE = "adaptiveSum")
     else if (lFun == "negbin_sentinel")
-      out = .Call("adapt_sum_callPrecomp",
-                  6L, params, eps, maxIter, logL, n0,
+      out = .Call("naive_sum_callPrecomp",
+                  6L, params, eps, maxIter, n0,
                   PACKAGE = "adaptiveSum")
     else if (lFun == "poisson_sentinel")
-      out = .Call("adapt_sum_callPrecomp",
-                  7L, params, eps, maxIter, logL, n0,
+      out = .Call("naive_sum_callPrecomp",
+                  7L, params, eps, maxIter, n0,
                   PACKAGE = "adaptiveSum")
     else if (lFun == "weird_series_constL")
-      out = .Call("adapt_sum_callPrecomp",
-                  8L, params, eps, maxIter, logL, n0,
+      out = .Call("naive_sum_callPrecomp",
+                  8L, params, eps, maxIter, n0,
                   PACKAGE = "adaptiveSum")
     else if (lFun == "weird_series")
-      out = .Call("adapt_sum_callPrecomp",
-                  9L, params, eps, maxIter, logL, n0,
+      out = .Call("naive_sum_callPrecomp",
+                  9L, params, eps, maxIter, n0,
                   PACKAGE = "adaptiveSum")
   } else if(is.function(lFun)) {
     f <- function(k, Theta) lFun(k, Theta)
 
-    out = .Call("adapt_sum",
+    out = .Call("naive_sum",
                 body(f), params, eps,
-                maxIter, logL,
+                maxIter,
                 n0, new.env(),
                 PACKAGE = "adaptiveSum")
   } else {
@@ -62,7 +62,7 @@ adapt_sum <- function(lFun, params = numeric(), eps = 1e-15, maxIter = 1e5,
   }
 
   n = out[[2]]
-  list(sum = matrixStats::logSumExp(out[[1]][1:(n+2)]),
+  list(sum = matrixStats::logSumExp(out[[1]][1:n]),
        n = n,
        out = out)
 
---
+++ R/smart_sum.R
@@ -0,0 +1,11 @@
+#' Test function
+#'
+#' Selects the faster method for sum approximation.
+#' @export
+smart_sum <- function(lFun, params = numeric(), eps = 1e-15, maxIter = 1e5,
+                      logL = -Inf, n0 = 0){
+
+  if (logL < log(0.5)) naive_sum(lFun, params, eps, maxIter, logL, n0)
+  else adapt_sum(lFun, params, eps, maxIter, logL, n0)
+
+}
