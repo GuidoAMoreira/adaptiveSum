6e4929b6bd1d46679a402b34ae4541feef9e260d
Guido Alberti Moreira <42647037+GuidoAMoreira@users.noreply.github.com>
2021-05-11 19:59:17 +0100

    Add files via upload

--- src/doublinc_sum_compiled.c
+++ src/doublinc_sum_compiled.c
@@ -10,7 +10,7 @@ SEXP doubling_sum_precomp(long double logFun(R_xlen_t k, double *Theta),
   // Declaration
   R_xlen_t n = 0, N, N_inc = N_start * c;
   long double maxA, lEps = log(eps), logFunVal[maxIter + 1],
-            partial, *checkStart = logFunVal, S;
+            partial = 0., *checkStart = logFunVal, S = 0., cc = 0., total = 0;
 
   logFunVal[n] = logFun(n0, params);
   while (!R_FINITE(logFunVal[n]) && n < (maxIter - 1))
@@ -23,35 +23,35 @@ SEXP doubling_sum_precomp(long double logFun(R_xlen_t k, double *Theta),
 
   // If too many iterations. Last iter is max.
   if (n == maxIter)
-    return retFun(logFunVal[n] +
-                  log1p(partial_logSumExp(logFunVal, maxIter - 1,
-                                          logFunVal[n])),
-                                          maxIter);
+  {
+    partial_logSumExp(logFunVal, maxIter - 1, logFunVal[n], &cc, 0, &total);
+    return retFun(logFunVal[n] + log1p(total), maxIter);
+  }
 
   // I know which is the max due to the stop criteria.
   // Assumed local max = global max.
   // 20 added to make calculations with more precision.
-  maxA = logFunVal[n - 1] + 20;
-  lEps -= maxA;
+  maxA = logFunVal[n - 1];
+  lEps -= maxA; // For the convergence checking
   while (n < N_inc && n < maxIter) // Complete the two first checkpoints.
     logFunVal[++n] = logFun(++n0, params);
   N = n == N_inc ? N_start : ((n - N_inc) / N_inc) * N_inc; // Second to last completed checkpoint.
-  partial = partial_logSumExp(logFunVal, N, maxA);
+  partial_logSumExp(logFunVal, N, maxA, &cc, 0, &partial);
   checkStart += N + 1;
   N = n == N_inc ? N_inc - N_start - 1 : N_inc - 1; // Last completed checkpoint.
-  S = partial_logSumExp(checkStart, N, maxA);
+  cc = 0.;
+  partial_logSumExp(checkStart, N, maxA, &cc, 0, &S);
 
   // Calculate the tail. Only loop once.
   while (log(S) >= lEps && n < maxIter)
   {
     partial += S;
-    for (N = 0, S = 0; N < N_inc; N++)
-      S += exp(logFun(++n0, params) - maxA);
+    for (N = 0, S = 0.; N < N_inc; N++)
+      KahanSum(&S, exp(logFun(++n0, params) - maxA), &cc);
     n += N_inc;
   }
-  partial += S;
 
-  return retFun(maxA + log(partial), n);
+  return retFun((double)(maxA + logl(partial + S)), n);
 }
 
 SEXP doubling_sum_callPrecomp(SEXP lF, SEXP params, SEXP epsilon, SEXP N_start,
--- src/doubling_sum.c
+++ src/doubling_sum.c
@@ -14,7 +14,8 @@ SEXP doubling_sum(SEXP logFun, SEXP params, SEXP epsilon, SEXP N_start_,
     INTEGER(n0_)[0], c = INTEGER(c_)[0], N_start =
     INTEGER(N_start_)[0], N, n = 0, N_inc = N_start * c;
   long double logFunVal[maxIter + 1], lEps = log(REAL(epsilon)[0]),
-    maxA, partial, *checkStart = logFunVal, S;
+    maxA, partial = 0., *checkStart = logFunVal, S = 0., cc = 0.,
+    ccc = 0., total = 0.;
   defineVar(install("Theta"), params, rho);
 
   defineVar(install("k"), Rf_ScalarInteger(n0), rho);
@@ -22,50 +23,56 @@ SEXP doubling_sum(SEXP logFun, SEXP params, SEXP epsilon, SEXP N_start_,
   while (!R_FINITE(logFunVal[n]) && n < (maxIter - 1))
   {
     defineVar(install("k"), Rf_ScalarInteger(++n0), rho);
-    logFunVal[++n] = feval(logFun,rho);
+    logFunVal[++n] = feval(logFun, rho);
   }
 
   // Find the maximum
   do
   {
     defineVar(install("k"), Rf_ScalarInteger(++n0), rho);
-    logFunVal[++n] = feval(logFun,rho);
-  } while (logFunVal[n] >= logFunVal[n - 1] && n <= (maxIter - 1));
+    logFunVal[++n] = feval(logFun, rho);
+  }
+  while (logFunVal[n] >= logFunVal[n - 1] && n <= (maxIter - 1));
 
   // If too many iterations. Last iter is max.
   if (n == maxIter)
-    return retFun(logFunVal[n] +
-                  log1p(partial_logSumExp(logFunVal, n - 1,
-                                          logFunVal[n])),
-                                          maxIter);
+  {
+    partial_logSumExp(logFunVal, maxIter - 1, logFunVal[n], &cc, 0, &total);
+    return retFun(logFunVal[n] + log1p(total), maxIter);
+  }
 
   // I know which is the max due to the stop criteria.
   // Assumed local max = global max.
   // 20 added to make calculations with more precision.
-  maxA = logFunVal[n - 1] + 20;
-  lEps -= maxA;
-  while (n < N_inc && n < maxIter) // Complete second bulk.
+  maxA = logFunVal[n - 1];
+  lEps -= maxA; // For the convergence checking
+  while (n < N_inc && n < maxIter) // Complete the two first checkpoints.
   {
     defineVar(install("k"), Rf_ScalarInteger(++n0), rho);
-    logFunVal[++n] = feval(logFun,rho);
+    logFunVal[++n] = feval(logFun, rho);
   }
   N = n == N_inc ? N_start : ((n - N_inc) / N_inc) * N_inc; // Second to last completed checkpoint.
-  partial = partial_logSumExp(logFunVal, N, maxA);
+  partial_logSumExp(logFunVal, N, maxA, &cc, 0, &partial);
   checkStart += N + 1;
   N = n == N_inc ? N_inc - N_start - 1 : N_inc - 1; // Last completed checkpoint.
-  S = partial_logSumExp(checkStart, N, maxA);
+  cc = 0.;
+  partial_logSumExp(checkStart, N, maxA, &cc, 0, &S);
 
+  // Calculate the tail. Only loop once.
   while (log(S) >= lEps && n < maxIter)
   {
-    partial += S;
-    for (N = 0, S = 0; N < N_inc; N++)
+    KahanSum(&partial, S, &ccc);
+    n0 += N_inc; // Going backwards for numerical stability
+    cc = 0.;
+    for (N = 0, S = 0.; N < N_inc; N++)
     {
-      defineVar(install("k"), Rf_ScalarInteger(++n0), rho);
-      S += exp(feval(logFun,rho) - maxA);
+      defineVar(install("k"), Rf_ScalarInteger(n0--), rho);
+      KahanSum(&S, exp(feval(logFun, rho) - maxA), &cc);
     }
+    n0 += N_inc;
     n += N_inc;
   }
-  partial += S;
+  KahanSum(&partial, S, &ccc);
 
-  return retFun(maxA + log(partial), n);
+  return retFun((double)(maxA + logl(partial)), n);
 }
